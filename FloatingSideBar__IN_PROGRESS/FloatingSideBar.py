45b8b77037709cb9ea87a91e7f0d0f18ed4cebd3
Venkata Santosh <santoshtvk16@gmail.com>
2024-10-06 14:42:29 +0530

    floating sidebar --delayed day2 --inprogress

--- FloatingSideBar__IN_PROGRESS/FloatingSideBar.py
+++ FloatingSideBar__IN_PROGRESS/FloatingSideBar.py
@@ -1,36 +1,61 @@
 import tkinter as tk
 from tkinter import messagebox, font
 
+small_w = 50
+small_h = 50
+big_w = 300
 bars = []
-
+#################################################### UNDER DEVELOPMENT
 
 class Sidebar(tk.Toplevel):
     def __init__(self, top):
         super().__init__(top)
+        self.icon_style = False
 
+        self.ws = self.winfo_screenwidth()  # width of the screen
+        self.hs = self.winfo_screenheight()  # height of the screen
+
+        # self.geometry(f"{small_w}x{small_h}+{int(self.ws - small_w)}+{int((self.hs / 2) - (small_h / 2))}")
+        self.overrideredirect(True)
         self.yclick = 0
         self.xclick = 0
-        self.config(bg='#838383')
+        self.config(bg='#888')
         self.attributes('-topmost', 'true')
-        self.resizable(True, True)
         self.custom_font = font.Font(size=15)
-
-        self.titlebar = tk.Frame(self, bg='#FFFF00', relief='flat', bd=2)
-        self.titlebar.bind('<Button-1>', self.get_pos)
-        self.titlebar.bind('<B1-Motion>', self.move_window)
-        self.titlebar.pack(fill=tk.X, expand=1, side=tk.TOP)
-
-        self.closebutton = tk.Label(self.titlebar, text='X', bg='#FFFF00', relief='flat', cursor='hand2',
-                                    font=self.custom_font)
-        self.closebutton.bind('<Button-1>', self.quit_window)
-        self.closebutton.pack(side=tk.RIGHT)
+        self.image = tk.PhotoImage(file="pull.png")
+        self.touch = tk.Button(self, image=self.image, cursor='hand2')
+        self.touch.bind('<Button-3>', self.toggle_window)
+        self.touch.bind('<Button-1>', self.get_pos)
+        self.touch.bind('<B1-Motion>', self.move_window)
+        self.touch["border"] = "0"
+        self.touch["bg"] = "white"
+        self.touch.pack(side=tk.TOP)
         bars.append(self)
 
-    def quit_window(self, event):
-        self.closebutton.config(relief='flat', bd=0)
-        if messagebox.askyesno('Delete Note?', 'Are you sure you want to delete this note?', parent=self):
-            self.destroy()
-            root.destroy()
+        self.listbox = tk.Listbox(root)
+        self.scrollbar = tk.Scrollbar(root)
+#
+        self.listbox.pack(side=tk.LEFT, fill=tk.BOTH)
+        self.scrollbar.pack(fill=tk.BOTH, expand=1)
+        self.geometry(f"{big_w}x{int(self.hs)}+{int(self.ws - big_w)}+{0}")
+        self.icon_style = True
+#
+        for values in range(1000000, 1000100):
+            self.listbox.insert(tk.END, values)
+
+        self.listbox.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.listbox.yview)
+
+    def toggle_window(self, event=None):
+        if self.icon_style:
+            self.scrollbar.pack_forget()
+            self.geometry(f"{small_w}x{small_h}+{int(self.ws - small_w)}+{int((self.hs / 2) - (small_h / 2))}")
+            self.icon_style = False
+        else:
+            self.listbox.pack(side=tk.LEFT, fill=tk.BOTH)
+            self.scrollbar.pack(fill=tk.BOTH, expand=1)
+            self.geometry(f"{big_w}x{int(self.hs)}+{int(self.ws - big_w)}+{0}")
+            self.icon_style = True
 
     def get_pos(self, event):
         self.xclick = event.x
@@ -38,65 +63,15 @@ class Sidebar(tk.Toplevel):
         print(self.xclick, self.yclick)
 
     def move_window(self, event):
-        self.geometry('+{0}+{1}'.format(event.x_root - self.xclick, event.y_root - self.yclick))
+        self.geometry('+{0}+{1}'.format(self.ws - small_w, event.y_root - self.yclick))  # event.x_root - self.xclick
 
     def on_closing(self):
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        if messagebox.askokcancel("Quit", "Click OK to proceed closing Side-Bar Application"):
             self.destroy()
             root.destroy()
 
 
 root = tk.Tk()
-
-def on_closing():
-    print("gone")
-    if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        for i in bars:
-            i.on_closing()
-        root.destroy()
-
-
-root.protocol("WM_DELETE_WINDOW", on_closing)
-
+root.withdraw()
 s = Sidebar(root)
 s.mainloop()
-
-
-# from tkinter import *
-# from tkinter import ttk
-#
-# # Create an instance of tkinter frame or window
-# win=Tk()
-#
-# # Set the size of the window
-# win.geometry("700x350")
-# win.overrideredirect(True)
-#
-# ws = win.winfo_screenwidth() # width of the screen
-# hs = win.winfo_screenheight() # height of the screen
-#
-# # calculate x and y coordinates for the Tk root window
-# x = (ws/2) - (700/2)
-# y = (hs/2) - (350/2)
-#
-# def moveMouseButton(e):
-#     x1 = e.x_root()
-#     y1 = e.y_root()
-#     x0 = e.x()
-#     y0 = e.y()
-#
-#     win.geometry("%s x %s" % ((x1 - x0), (y1 - y0)))
-#
-# win.geometry('%dx%d+%d+%d' % (700, 350, x, y))
-#
-# # Add a Label widget
-# label=Label(win,text="Grab the lower-right corner to resize the window")
-# label.pack(side="top", fill="both", expand=True)
-#
-# # Add the gripper for resizing the window
-# grip=ttk.Sizegrip()
-# grip.place(relx=1.0, rely=1.0, anchor="se")
-# grip.lift(label)
-# grip.bind("<B1-Motion>", moveMouseButton)
-#
-# win.mainloop()
